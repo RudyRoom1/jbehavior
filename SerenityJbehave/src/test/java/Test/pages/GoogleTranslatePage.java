c392f7b6837c609dbb2baad2e0988f6f91085ce1
Rudyroom1 <rudy.crypt@gmail.com>
2019-02-25 02:52:12 +0200

    Change List library

--- SerenityJbehave/src/test/java/Test/pages/GoogleTranslatePage.java
+++ SerenityJbehave/src/test/java/Test/pages/GoogleTranslatePage.java
@@ -1,11 +1,11 @@
 package Test.pages;
 
-import io.vavr.collection.List;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 
+import java.util.List;
 
 
 @DefaultUrl("https://translate.google.com")
