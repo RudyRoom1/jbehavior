f45c44e16929f85bdc2d298f391a997266f268bf
Rudyroom1 <rudy.crypt@gmail.com>
2019-02-26 14:13:50 +0200

    1

--- src/test/java/Test/AcceptanceTestSuite.java
+++ src/test/java/Test/AcceptanceTestSuite.java
@@ -4,8 +4,7 @@ import net.serenitybdd.jbehave.SerenityStories;
 import net.serenitybdd.jbehave.annotations.Metafilter;
 import net.thucydides.core.webdriver.Configuration;
 
-import static net.thucydides.core.ThucydidesSystemProperty.WEBDRIVER_DRIVER;
-import static net.thucydides.core.ThucydidesSystemProperty.WEBDRIVER_PROVIDED_TYPE;
+import static net.thucydides.core.ThucydidesSystemProperty.*;
 
 @Metafilter("+debug")
 public class AcceptanceTestSuite extends SerenityStories {
@@ -16,5 +15,6 @@ public class AcceptanceTestSuite extends SerenityStories {
         configuration.setIfUndefined(WEBDRIVER_DRIVER.getPropertyName(), "provided");
         configuration.setIfUndefined(WEBDRIVER_PROVIDED_TYPE.getPropertyName(), "mydriver");
         configuration.setIfUndefined("webdriver.provided.mydriver", "Test.drivers.ChromeWebDriver");
+        configuration.setIfUndefined(SERENITY_OUTPUT_DIRECTORY.getPropertyName(), "target/site/serenity");
     }
 }
