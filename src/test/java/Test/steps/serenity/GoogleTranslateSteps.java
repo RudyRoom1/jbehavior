cc9630dae1df6d695e09b80ec76d4195aff6ebeb
Rudyroom1 <rudy.crypt@gmail.com>
2019-02-26 15:51:06 +0200

    Add waiter.

--- src/test/java/Test/steps/serenity/GoogleTranslateSteps.java
+++ src/test/java/Test/steps/serenity/GoogleTranslateSteps.java
@@ -2,9 +2,13 @@ package Test.steps.serenity;
 
 import Test.pages.GoogleTranslatePage;
 import net.serenitybdd.core.Serenity;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.Step;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleTranslateSteps {
 
@@ -17,6 +21,8 @@ public class GoogleTranslateSteps {
 
     @Step
     public void selectLanguages(String firstLang, String secondLang) {
+        WebDriverWait wait = new WebDriverWait(googleTranslatePage.getDriver(),5);
+        wait.until(ExpectedConditions.visibilityOf(googleTranslatePage.getMoreButtonEng()));
         googleTranslatePage.getMoreButtonEng().click();
         googleTranslatePage.getEnterEngLang().sendKeys(firstLang);
         googleTranslatePage.getEnterEngLang().sendKeys(Keys.ENTER);
@@ -36,19 +42,19 @@ public class GoogleTranslateSteps {
         Assert.assertEquals(checkWord, googleTranslatePage.getResultField().getText());
     }
 
-//    @Step
+    //    @Step
 //    public void checkCountOfWords(String expectedCount){
 //        String actualCount = googleTranslatePage.getCountField().getText();
 //        actualCount = actualCount.substring(0, actualCount.indexOf('/'));
 //        Assert.assertEquals(expectedCount, actualCount);
 //    }
     @Step
-    public void checkCountOfWords(){
+    public void checkCountOfWords() {
         String actualText = googleTranslatePage.getCountField().getText();
         int actualCount = Integer.parseInt(actualText.substring(0, actualText.indexOf('/')));
-        String word = (String)Serenity.getCurrentSession().get("enteredWord");
-        int lenthOfWord =word.length();
-        Assert.assertEquals(lenthOfWord, actualCount);
+        String word = (String) Serenity.getCurrentSession().get("enteredWord");
+        int length = word.length();
+        Assert.assertEquals(length, actualCount);
     }
 
     @Step
--- target/test-classes/stories/consult_dictionary/GoogleTranslateTest1.story
+++ target/test-classes/stories/consult_dictionary/GoogleTranslateTest1.story
@@ -8,9 +8,6 @@ So that I can achieve a business goal
 Meta:
 @debug
 
-
 Given the user is on the google translate page 'English' 'Ukrainian'
-
 When the user looks up the translation of the word 'apple'
-
 Then they should see the translate of word 'яблуко'
